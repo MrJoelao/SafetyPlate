08313670a730b0051022efb79a89389d4fc066bc
joelao <joel.stephan.email@gmail.com>
2025-02-21 10:41:39 +0100

    aggiunta dell'interfaccia Food per la gestione delle informazioni sugli alimenti

---
+++ types/food.ts
@@ -0,0 +1,12 @@
+export interface Food {
+  id: string;
+  name: string;
+  score: number;
+  defaultUnit: string;
+  nutritionPer100g?: {
+    calories?: number;
+    proteins?: number;
+    carbs?: number;
+    fats?: number;
+  };
+}
