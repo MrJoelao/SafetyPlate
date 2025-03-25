1369f3ea36a88642db7b2e84e8381668a2129163
joelao <joel.stephan.email@gmail.com>
2025-03-25 11:29:31 +0100

    componenti per le icone

---
+++ components/ui/icons/IconSymbol.tsx
@@ -0,0 +1,20 @@
+// This file is a fallback for using MaterialIcons on Android and web.
+
+import type React from "react"
+import { MaterialIcons } from "@expo/vector-icons"
+import type { MaterialIcons as MaterialIconsType } from "@expo/vector-icons/MaterialIcons"
+
+export type IconType = keyof typeof MaterialIconsType.glyphMap
+
+interface IconSymbolProps {
+  name: IconType
+  size?: number
+  color?: string
+  style?: any
+  weight?: string
+}
+
+export const IconSymbol: React.FC<IconSymbolProps> = ({ name, size = 24, color = "#000", style, weight }) => {
+  return <MaterialIcons name={name} size={size} color={color} style={style} />
+}
+
